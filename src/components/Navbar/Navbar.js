abc869baa97c55a7c5a2c2e03a549cf49aad7847
James <jamesdev4you@gmail.com>
2024-04-01 17:46:56 -0400

    dropdown menu

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -2,7 +2,6 @@ import * as React from 'react';
 import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
 import CssBaseline from '@mui/material/CssBaseline';
-import Divider from '@mui/material/Divider';
 import Drawer from '@mui/material/Drawer';
 import IconButton from '@mui/material/IconButton';
 import List from '@mui/material/List';
@@ -15,20 +14,59 @@ import '../../navlink.css';
 import '../../index.css';
 import { Outlet } from 'react-router-dom';
 import { MyHeaderButton } from '../styledComponents';
+import MenuItem from '@mui/material/MenuItem';
+import Menu from '@mui/material/Menu';
+import Typography from '@mui/material/Typography';
+import Fade from '@mui/material/Fade';
+import Button from '@mui/material/Button';
+import PopupState, { bindTrigger, bindMenu } from 'material-ui-popup-state';
 
 const drawerWidth = 240;
 const navItems = [
   { text: 'Home', href: '/' },
-  { text: 'About', href: '/about' },
-  { text: 'Services', href: '/services' },
-  { text: 'Price Estimate', href: '/priceestimate' },
+  {
+    text: 'About',
+    href: '/about',
+    dropdownItems: [
+      { text: 'ABOUT', href: '/about' },
+      { text: 'Blogs', href: '/blog' },
+      { text: 'Newsletter', href: '/newsletter' },
+      { text: 'FAQ', href: '/faq' },
+    ],
+  },
+  {
+    text: 'Services',
+    href: '/services',
+    dropdownItems: [
+      { text: 'SERVICES', href: '/services' },
+      { text: 'Air Conditioning', href: '/airconditioning' },
+      { text: 'Heating', href: '/heating' },
+      { text: 'Thermostat', href: '/thermostat' },
+      { text: 'Ductwork', href: '/ductwork' },
+      { text: 'Insulation', href: '/insulation' },
+    ],
+  },
+  { text: 'Contact', href: '/contact' },
+  { text: 'Estimate', href: '/priceestimate' },
 ];
 
 export default function NavBar() {
   const [mobileOpen, setMobileOpen] = React.useState(false);
+  const [anchorEl, setAnchorEl] = React.useState(null);
 
   const handleDrawerToggle = () => {
     setMobileOpen((prevState) => !prevState);
+  };
+
+  const open = Boolean(anchorEl);
+
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+    console.log('help jesus', event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
   };
 
   const drawer = (
@@ -37,7 +75,6 @@ export default function NavBar() {
       sx={{ textAlign: 'center', backgroundColor: 'primary.main' }}
     >
       <img src={Logo} alt='yooo' style={{ height: '145px', width: '150px' }} />
-
       <List sx={{ backgroundColor: 'primary.main' }}>
         {navItems.map((item) => (
           <ListItem key={item.text} sx={{ margin: '15px 0px' }} disablePadding>
@@ -76,8 +113,8 @@ export default function NavBar() {
           <Box
             sx={{
               display: 'block',
-              height: '70px',
-              width: '70px',
+              height: '100px',
+              width: '100px',
               marginLeft: {
                 xs: 'auto',
                 sm: 'auto',
@@ -96,21 +133,96 @@ export default function NavBar() {
           />
           <Box
             sx={{
-              display: { xs: 'none', sm: 'none', md: 'none', lg: 'block' },
+              display: {
+                xs: 'none',
+                sm: 'none',
+                md: 'none',
+                lg: 'flex',
+                xl: 'flex',
+              },
               marginLeft: {
                 xs: 'none',
                 sm: 'none',
                 md: '80px',
-                lg: '0px',
-                xl: '550px',
+                lg: '15%',
+                xl: '25%',
               },
               margin: 'auto',
+              gap: '3em',
             }}
           >
-            {navItems.map((item) => (
-              <NavLink key={item.href} className='menu_link' to={item.href}>
-                {item.text}
-              </NavLink>
+            {navItems.map((item, index) => (
+              <ListItem
+                key={item.text}
+                sx={{ margin: '15px 0px' }}
+                disablePadding
+              >
+                {item.dropdownItems ? (
+                  <>
+                    <PopupState variant='popover' popupId='demo-popup-menu'>
+                      {(popupState) => (
+                        <React.Fragment>
+                          <Button
+                            variant='contained'
+                            {...bindTrigger(popupState)}
+                          >
+                            {item.text}
+                          </Button>
+                          <Menu
+                            id={item.text}
+                            {...bindMenu(popupState)}
+                            anchorOrigin={{
+                              vertical: 'top',
+                              horizontal: '0px',
+                            }}
+                            sx={{
+                              '& .MuiPaper-root': {
+                                backgroundColor: 'black', // Background color of the menu
+                              },
+                            }}
+                          >
+                            {item.dropdownItems.map((dropdownItem) => (
+                              <MenuItem
+                                key={dropdownItem.text}
+                                onClick={popupState.close}
+                                sx={{
+                                  backgroundColor: 'black',
+                                  paddingTop: '0px',
+                                  marginLeft: '1px',
+                                  marginBottom: '15px',
+                                }}
+                              >
+                                <NavLink
+                                  to={dropdownItem.href}
+                                  style={{
+                                    textDecoration: 'none',
+                                    color: 'white',
+                                    fontSize: '14px',
+                                    marginRight: 'auto',
+                                    fontFamily: 'Roboto',
+                                  }}
+                                >
+                                  {dropdownItem.text}
+                                </NavLink>
+                              </MenuItem>
+                            ))}
+                          </Menu>
+                        </React.Fragment>
+                      )}
+                    </PopupState>
+                  </>
+                ) : (
+                  <Button variant='contained'>
+                    <NavLink
+                      key={item.href}
+                      className='menu_link'
+                      to={item.href}
+                    >
+                      {item.text}
+                    </NavLink>
+                  </Button>
+                )}
+              </ListItem>
             ))}
           </Box>
           <Box
