c565cfda51fbe98a4f8a6198a1e5ab11aab084c2
James <jamesdev4you@gmail.com>
2024-04-02 12:10:38 -0400

    about page next is blogs

--- src/components/About/aboutBlogs/AboutBlogs.js
+++ src/components/About/aboutBlogs/AboutBlogs.js
@@ -1,7 +1,49 @@
 import React from 'react';
+import Box from '@mui/material/Box';
+import AboutBlogsText from './AboutBlogsText';
+import AboutBlogsGrid from './AboutBlogsGrid';
 
 const AboutBlogs = () => {
-  return <div>AboutBlogs</div>;
+  return (
+    <Box
+      sx={{
+        width: { xl: '70%', lg: '80%', md: '90%', sm: '100%', xs: '100%' },
+        height: {
+          xl: '65vh',
+          lg: '80%',
+          md: '80%',
+          sm: '90%',
+          xs: '50%',
+        },
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: {
+          xl: 'space-between',
+          lg: 'space-between',
+          md: 'space-between',
+          sm: 'center',
+          xs: 'center',
+        },
+        alignItems: {
+          xl: 'center',
+          lg: 'start',
+          md: 'start',
+          sm: 'center',
+          xs: 'center',
+        },
+        gap: {
+          xl: '0em',
+          lg: '3em',
+          md: '1em',
+          sm: '2em',
+          xs: '2em',
+        },
+      }}
+    >
+      <AboutBlogsText />
+      <AboutBlogsGrid />
+    </Box>
+  );
 };
 
 export default AboutBlogs;
--- src/components/About/aboutBlogs/AboutBlogsText.js
+++ src/components/About/aboutBlogs/AboutBlogsText.js
@@ -1,7 +1,31 @@
 import React from 'react';
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
 
 const AboutBlogsText = () => {
-  return <div>A</div>;
+  return (
+    <Box
+      sx={{
+        height: '100%',
+        width: '70%',
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        gap: '1em',
+      }}
+    >
+      <Typography
+        variant='h1'
+        sx={{ width: '100%', textAlign: 'center', fontWeight: 'bold' }}
+      >
+        Check Out Our Blogs
+      </Typography>
+      <Typography variant='h6' sx={{ width: '60%', textAlign: 'center' }}>
+        lorem ipsum lorem ipsum lorem ipsum ipsum lorem ipsum lorem ipsum lorem
+        lorem lorem ipsum lorem
+      </Typography>
+    </Box>
+  );
 };
 
 export default AboutBlogsText;
