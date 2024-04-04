cdc493b58f9fc8dd50b8e5b37fd9728d6d78b1d3
James <jamesdev4you@gmail.com>
2024-04-04 11:02:36 -0400

    cms updated for services

--- gunnblog/schemaTypes/index.js
+++ gunnblog/schemaTypes/index.js
@@ -3,5 +3,6 @@ import category from './category'
 import post from './post'
 import author from './author'
 import howto from './howtoread'
+import services from './services'
 
-export const schemaTypes = [post, author, category, blockContent, howto]
+export const schemaTypes = [post, author, category, blockContent, howto, services]
---
+++ gunnblog/schemaTypes/services.js
@@ -0,0 +1,58 @@
+import {defineField, defineType} from 'sanity'
+
+export default defineType({
+  name: 'services',
+  title: 'Services',
+  type: 'document',
+  fields: [
+    defineField({
+      name: 'title',
+      title: 'Title',
+      type: 'string',
+    }),
+    defineField({
+      name: 'headerTitle',
+      title: 'Header Title',
+      type: 'string',
+    }),
+    defineField({
+      name: 'headerDesc',
+      title: 'Header Description',
+      type: 'string',
+    }),
+    defineField({
+      name: 'headerImage',
+      title: 'Header image',
+      type: 'image',
+      options: {
+        hotspot: true,
+      },
+    }),
+    defineField({
+      name: 'slug',
+      title: 'Slug',
+      type: 'slug',
+      options: {
+        source: 'title',
+        maxLength: 96,
+      },
+    }),
+    defineField({
+      name: 'body',
+      title: 'Body',
+      type: 'blockContent',
+    }),
+  ],
+
+  preview: {
+    select: {
+      title: 'title',
+      author: 'author.name',
+      media: 'mainImage',
+    },
+    prepare(selection) {
+      const {author} = selection
+      return {...selection, subtitle: author && `by ${author}`}
+    },
+  },
+})
