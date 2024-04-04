1cb341e96ef788dfdb528ac6d9b6e3ffabf07b69
James <jamesdev4you@gmail.com>
2024-04-04 11:14:41 -0400

    services header text + pic

--- src/components/Services/ServicesHeader/ServicesPicHeader.js
+++ src/components/Services/ServicesHeader/ServicesPicHeader.js
@@ -1,7 +1,63 @@
 import React from 'react';
+import Box from '@mui/material/Box';
+import GunnBros from '../../../assets/pictures/logoBlack.png';
 
 const ServicesPicHeader = () => {
-  return <div>ServicesPicHeader</div>;
+  return (
+    <Box
+      sx={{
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+        width: { xl: '40%', lg: 'auto', md: 'auto', sm: '90%', xs: '100%' },
+        height: {
+          xl: '350px',
+          lg: '250px',
+          md: '200px',
+          sm: '200px',
+          xs: '200px',
+        },
+        backgroundColor: {
+          xl: 'none',
+          lg: 'none',
+          md: 'none',
+          sm: 'none',
+          xs: 'none',
+        },
+        borderRadius: '15px',
+      }}
+    >
+      <Box
+        sx={{
+          backgroundImage: `url(${GunnBros})`,
+          width: {
+            xl: '350px',
+            lg: '250px',
+            md: '200px',
+            sm: '200px',
+            xs: '200px',
+          },
+          height: {
+            xl: '350px',
+            lg: '250px',
+            md: '200px',
+            sm: '200px',
+            xs: '200px',
+          },
+          backgroundSize: 'cover',
+          backgroundRepeat: 'none',
+          backgroundPosition: 'center',
+          borderRadius: {
+            xl: '15px',
+            lg: '15px',
+            md: '15px',
+            sm: '0px',
+            xs: '0px',
+          },
+        }}
+      />
+    </Box>
+  );
 };
 
 export default ServicesPicHeader;
--- src/components/Services/ServicesHeader/ServicesTextHeader.js
+++ src/components/Services/ServicesHeader/ServicesTextHeader.js
@@ -1,7 +1,95 @@
 import React from 'react';
+import Typography from '@mui/material/Typography';
+import Box from '@mui/material/Box';
+import { MyNormalButton } from '../../styledComponents';
 
 const ServicesTextHeader = () => {
-  return <div>ServicesTextHeader</div>;
+  return (
+    <Box
+      sx={{
+        width: { xl: '50%', lg: '50%', md: '60%', sm: '90%', xs: '100%' },
+        height: 'auto',
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: {
+          xl: 'start',
+          lg: 'start',
+          md: 'start',
+          sm: 'center',
+          xs: 'center',
+        },
+        justifyContent: 'start',
+      }}
+    >
+      <Typography
+        variant='h1'
+        sx={{ fontWeight: 'bold', textAlign: { xs: 'center', sm: 'none' } }}
+      >
+        The Gunn Brothers
+      </Typography>
+      <Typography
+        variant='h2'
+        sx={{ fontWeight: 'bold', textAlign: { xs: 'center', sm: 'none' } }}
+      >
+        of Tampa Bay
+      </Typography>
+      <Typography
+        variant='subtitle1'
+        sx={{
+          textAlign: {
+            xl: 'left',
+            lg: 'left',
+            md: 'left',
+            sm: 'center',
+            xs: 'center',
+          },
+          marginTop: '1em',
+          marginBottom: '1em',
+          fontWeight: 'bold',
+        }}
+      >
+        Find your all for one place to gain buzz and traction online! Try out
+        Hive's AI features for free today!
+      </Typography>
+      <Box
+        sx={{
+          display: {
+            xl: 'flex',
+            lg: 'flex',
+            md: 'flex',
+            sm: 'none',
+            xs: 'none',
+          },
+          width: '100%',
+          justifyContent: 'space-between',
+        }}
+      >
+        <MyNormalButton children='Learn More' />
+        <MyNormalButton children='Request Service' />
+      </Box>
+      <Box
+        sx={{
+          display: {
+            xl: 'none',
+            lg: 'none',
+            md: 'none',
+            sm: 'flex',
+            xs: 'flex',
+          },
+          width: '100%',
+          justifyContent: {
+            xl: 'start',
+            lg: 'start',
+            md: 'start',
+            sm: 'center',
+            xs: 'center',
+          },
+        }}
+      >
+        <MyNormalButton children='Learn More' />
+      </Box>
+    </Box>
+  );
 };
 
 export default ServicesTextHeader;
